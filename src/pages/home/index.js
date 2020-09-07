2336ba07c2d5caf291d373e46bcc7d94b06483c8
Jonathan Lima <jonathan.lima@gs.liferay.com>
2020-09-07 01:57:22 -0300

    ft-company - Develop company section layout

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,15 +3,28 @@ import './index.scss';
 
 import MainHeader from '../../components/mainHeader';
 import SearchHeader from '../../components/searchHeader';
+import Company from '../../components/company';
 
 
 function Home() {
     const [viewHeader, setViewHeader] = useState(true);
+    const [hasCompany, setHasCompany] = useState(false);
     return (
         <>
             <header className="header d-flex justify-content-around align-items-center">
                 {viewHeader ? <MainHeader /> : <SearchHeader />}
             </header>
+
+            <div className="container">
+                {
+                    viewHeader && !hasCompany ?
+                        <p className="initial-disclaimer">Clique na busca para iniciar.</p>
+                        :
+                        <div className="row my-5">
+                            {hasCompany ? <Company /> : <p className="disclaimer">Nenhuma empresa foi encontrada para a busca realizada.</p>}
+                        </div>
+                }
+            </div>
         </>
     );
 }
