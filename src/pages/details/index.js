4f7c57479d024a82505aff317135ac7656c79da3
Jonathan Lima <jonathan.lima@gs.liferay.com>
2020-09-08 14:57:49 -0300

    feature-detail-card - Create inner page

---
+++ src/pages/details/index.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { MdKeyboardBackspace } from 'react-icons/md';
+import './index.scss';
+
+function Details() {
+    const [detail, setDetail] = useState();
+    const [counter, setCounter] = useState(0);
+
+    const retrieveData = () => {
+        let dataRequest = localStorage.getItem('data');
+        dataRequest = JSON.parse(dataRequest);
+        let id = window.location.href.split("?id=")[1];
+        let myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("access-token", dataRequest.token);
+        myHeaders.append("client", dataRequest.client);
+        myHeaders.append("uid", dataRequest.uid);
+
+        let requestOptions = {
+            method: 'GET',
+            headers: myHeaders
+        };
+
+        fetch(`https://empresas.ioasys.com.br/api/v1/enterprises/${id}`, requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                setDetail(result);
+            })
+            // .then(() => { setserviceRequest(false) })
+            .catch(error => {
+                alert('Ocorreu algum problema tente novamente em alguns instantes')
+                console.log('error', error)
+            });
+    };
+
+    useEffect(() => {
+        retrieveData();
+    }, [counter])
+
+    return (
+        <>
+            <header className="header d-flex justify-content-start pl-5 text-white align-items-center font-weight-bold">
+                <Link to="/home" className="text-white"><MdKeyboardBackspace /></Link>
+                {detail ?
+                    <span className="pl-5">{detail.enterprise.enterprise_name}</span>
+
+                    : null}
+            </header >
+
+            {detail ?
+                <div className="container">
+                    <div className="row">
+                        <div className="card-box mt-5">
+                            <div className="image-box">
+                                <img className="image" src={`https://empresas.ioasys.com.br${detail.enterprise.photo}`} alt="" />
+                            </div>
+
+                            <p className="description">
+                                {detail.enterprise.description}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                : null}
+        </>
+    );
+}
+
+export default Details;
